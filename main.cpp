1c0a24499b516764d51ecd4107c61a97617f20a3
Efe Deniz <88078414+kanemoda@users.noreply.github.com>
2024-05-02 21:00:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,7 @@ int main(int argc, char const *argv[])
 {
     bigInt num = bigInt("99");
     bigInt num2 = bigInt("99");
-    num.print();
-    num.add(num2);
-    num.print();
-    num.subtract(num2);
+    num.multiply(num2);
     num.print();
     return 0;
 }
