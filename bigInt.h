e7147df51c3be675a41b08fdbe6d2887e3969142
Efe Deniz <88078414+kanemoda@users.noreply.github.com>
2024-05-07 00:38:22 +0300

    Update bigInt.h

--- bigInt.h
+++ bigInt.h
@@ -3,14 +3,15 @@
 #include <vector>
 #include <string>
 #include <complex>
-using namespace::std;
+using namespace ::std;
 
 class bigInt
 {
 private:
     vector<int> number;
-    vector<complex<double>> fft(const vector<complex<double>>& a);
-    vector<complex<double>> ifft(const vector<complex<double>>& a);
+    vector<complex<double>> fft(const vector<complex<double>> &a);
+    vector<complex<double>> ifft(const vector<complex<double>> &a);
+
 public:
     void print();
     bigInt(string num);
@@ -18,4 +19,8 @@ public:
     void subtract(bigInt num);
     void multiply(bigInt num);
     void divide(bigInt num);
+    bool operator>(const bigInt &num) const;
+    bool operator<(const bigInt &num) const;
+    bool operator==(const bigInt &num) const;
+    void exponentiation(int exp);
 };
