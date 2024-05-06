41d5f201fa727c3ce87f4183753a08ceb95d3461
Efe Deniz <88078414+kanemoda@users.noreply.github.com>
2024-05-07 00:39:03 +0300

    Update main.cpp
    Exponentiation test

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "bigInt.h"
-using namespace::std;
+using namespace ::std;
 
 int main(int argc, char const *argv[])
 {
-    bigInt num = bigInt("99");
-    bigInt num2 = bigInt("99");
-    num.multiply(num2);
-    num.print();
-    return 0;
+    bigInt a("123456789");
+    bigInt b("987654321");
+    bigInt modulus("1000000007");
+
+    // Exponentiation: a^3
+    a.exponentiation(3);
+    cout << "a^3 = ";
+    a.print(); // Output: a^3 = 1881676371789154860897069
 }
